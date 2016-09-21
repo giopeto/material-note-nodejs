2f07c2e8f2c636694eeac7822831f68382b177bb
giopeto <giopeto0@yahoo.com>
2016-09-21 17:20:45 +0300

    openshift

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
     remoteUrl : process.env.OPENSHIFT_MONGODB_DB_URL ? process.env.OPENSHIFT_MONGODB_DB_URL + "material-note" : "",
     //remoteUrl: 'mongodb://localhost/material-note'
+
 };
