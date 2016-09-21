485629f9f64f0412b601e9a28bc3290a8c19a891
giopeto <giopeto0@yahoo.com>
2016-09-21 18:32:21 +0300

    First commit

--- config/db.js
+++ config/db.js
@@ -2,4 +2,5 @@ module.exports = {
     remoteUrl : process.env.OPENSHIFT_MONGODB_DB_URL ? process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME : ""
     //remoteUrl: 'mongodb://localhost/material-note'
 
+
 };
