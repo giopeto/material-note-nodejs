0eb962a299654c6965eae63c2210eb7b55f07b30
giopeto <giopeto0@yahoo.com>
2016-09-21 15:05:00 +0300

    home

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //remoteUrl : process.env.OPENSHIFT_MONGODB_DB_URL ? process.env.OPENSHIFT_MONGODB_DB_URL + "material-note" : "",
+    remoteUrl : process.env.OPENSHIFT_MONGODB_DB_URL ? process.env.OPENSHIFT_MONGODB_DB_URL + "material-note" : "",
     localUrl: 'mongodb://localhost/material-note'
 };
