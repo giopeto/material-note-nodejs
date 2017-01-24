758e15140437ed196b0d41bcc42abe10e165d2ab
giopeto <giopeto0@yahoo.com>
2017-01-24 18:41:50 +0200

    Add local db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
-    remoteUrl : process.env.OPENSHIFT_MONGODB_DB_URL ? process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME : ""
-    //remoteUrl: 'mongodb://localhost/material-note'
+    remoteUrl : process.env.OPENSHIFT_MONGODB_DB_URL ? process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME : 'mongodb://localhost/material-note' 
+   //remoteUrl: 'mongodb://localhost/material-note'
 
 
 };
